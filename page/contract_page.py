47b5a7dce3bf50ae90e84fc42af270cbf7be57a8
quyvuduc123456@gmail.com <GaBigQa!@12>
2026-09-06 19:12:18 +0700

    update tcs2

--- page/contract_page.py
+++ page/contract_page.py
@@ -33,7 +33,7 @@ class ContractPage(BasePage):
     CONTRACT_ROW = "//table[@id='contracts']//tbody//tr"  # TODO: Add locator strategy and value
     CONTRACT_HEADERS = "#contracts thead th"
     CONTRACT_TABLE_ROWS = "#contracts tbody tr"
-    CONTRACT_SUBJECT_LINK = "//table[@id='contracts']//tbody//tr/td[2]"  # TODO: Add locator strategy and value
+    CONTRACT_SUBJECT_LINK = "(//table[@id='contracts']//tbody//tr/td[2])[1]"  # TODO: Add locator strategy and value
     EDIT_BUTTON = "//a[.='Edit ']"  # TODO: Add locator for the Edit action revealed on contract hover
     DELETE_BUTTON = "//a[@class='text-danger _delete']"  # TODO: Add locator strategy and value
     DELETE_SUCCESS_TOAST = "#alert_float_1"  # TODO: Add locator strategy and value
